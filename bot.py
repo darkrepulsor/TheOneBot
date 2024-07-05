87b5ad770486ce5f6aa350036e169561b0c98da1
darkrepulsor <abraao.lima@live.com>
2024-07-04 23:41:12 -0300

    Corrigindo o main

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater,CommandHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder,CommandHandler
 from controllers.bot_controller import handle_quote
 import os
 from dotenv import load_dotenv
@@ -16,15 +17,11 @@ load_dotenv()
 token = os.getenv("TELEGRAM_TOKEN")
 
 def main():
-    updater = Updater(token=token,use_context=True)
-    dispatcher = updater.dispatcher
+    app = ApplicationBuilder().token(token).build()
 
-    quote_handler = CommandHandler('quote', handle_quote)
-    logger = logging.info('Recebeu o comando e passou para o controller.')
-    dispatcher.add_handler(quote_handler)
-
-    updater.start_polling()
-    updater.idle()
+    app.add_handler(CommandHandler('quote',handle_quote))
+    logger.info('Recebeu o comando e passou para o controller.')
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
